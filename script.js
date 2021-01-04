b194dcef346c498ea615c904abd55a6385c87c93
adrianepedroso <adrianeped1995@gmail.com>
2021-01-04 20:42:20 -0300

    fix toggle

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const toggleCarreira = () => {
     const carreira = document.getElementById("carreira").classList.toggle("none");
+    const pessoal = document.getElementById("pessoal").classList.add("none");
 }
 
 const togglePessoal = () => {
     const pessoal = document.getElementById("pessoal").classList.toggle("none");
+    const carreira = document.getElementById("carreira").classList.add("none");
 }
 
 const hideAll = () => {
