7c8b118600b2fbbdd4b4e420e009a0365997a16f
adrianepedroso <adrianeped1995@gmail.com>
2021-01-05 18:00:33 -0300

    add new layout

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const toggleCarreira = () => {
     const carreira = document.getElementById("carreira").classList.toggle("none");
     const pessoal = document.getElementById("pessoal").classList.add("none");
+    const photo = document.getElementById("photo").classList.add("none");
 }
 
 const togglePessoal = () => {
     const pessoal = document.getElementById("pessoal").classList.toggle("none");
     const carreira = document.getElementById("carreira").classList.add("none");
+    const photo = document.getElementById("photo").classList.add("none");
 }
 
 const hideAll = () => {
     const carreira = document.getElementById("carreira").classList.add("none");
     const pessoal = document.getElementById("pessoal").classList.add("none");
+    const photo = document.getElementById("photo").classList.remove("none");
 }
